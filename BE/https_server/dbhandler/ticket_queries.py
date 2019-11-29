33294f90abd08fe05d8b507fbbc6f4d06c10b953
cattylex <xpesko00@vutbr.cz>
2019-11-29 22:41:49 +0100

    ticket task POST

--- BE/https_server/dbhandler/ticket_queries.py
+++ BE/https_server/dbhandler/ticket_queries.py
@@ -62,7 +62,6 @@ def insert_comment(db_write):
     safe_exec.write(con, query, placeholders)
     con.close()
 
-
 def get_author_name(id):
     con = sqlite3.connect(DATABASE)
 
@@ -88,6 +87,22 @@ def get_ticket_tasks(id):
     resp = cur.fetchall()
     con.close()
     return resp
+
+def insert_task(db_write):
+    con = sqlite3.connect(DATABASE)
+
+    placeholders = (db_write['ticket'],
+                    db_write['author'],
+                    db_write['name'],
+                    db_write['descr'],
+                    db_write['state'],
+                    db_write['ewt'],
+                    db_write['ats'],
+                    db_write['created'])
+    query = 'INSERT INTO task (ticket, author, name, descr, state, ewt, ats, created) VALUES (?,?,?,?,?,?,?,?)'
+
+    safe_exec.write(con, query, placeholders)
+    con.close()
 
 def tickets_tasks_detail_GET(t_id, id):
     con = sqlite3.connect(DATABASE)
--- BE/https_server/restapi/mh_tickets.py
+++ BE/https_server/restapi/mh_tickets.py
@@ -180,11 +180,11 @@ def tickets_tasks_GET(**kwargs):
     tasks = dbhandler.get_ticket_tasks(id)
 
     for row in tasks:
-        help_response['id'] = row[TICKET_ID]
-        help_response['author_id'] = row[TICKET_AUTHOR]
-        help_response['author_nickname'] = dbhandler.get_author_name(row[TICKET_AUTHOR])
-        help_response['name'] = row[TICKET_NAME]
-        help_response['state'] = row[TICKET_STATE]
+        help_response['id'] = row[TASK_ID]
+        help_response['author_id'] = row[TASK_AUTHOR]
+        help_response['author_nickname'] = dbhandler.get_author_name(row[TASK_AUTHOR])
+        help_response['name'] = row[TASK_NAME]
+        help_response['state'] = row[TASK_STATE]
         response.append(help_response)
         help_response = {}
 
@@ -197,6 +197,28 @@ def tickets_tasks_GET(**kwargs):
 
     # return Response('<h1>tickets_tasks_GET ' + id + '</h1>', mimetype='text/html')
     return jsonify(response)
+
+@utility.add_required_headers
+def tickets_tasks_POST(**kwargs):
+    db_write = {}
+
+    try:
+        db_write['author'] = request.json['author']
+        db_write['name'] = request.json['name']
+        db_write['descr'] = request.json['descr']
+        db_write['ewt'] = request.json['ewt']
+    except KeyError:
+        errorhandler.send_error(404, 'key value missing')
+    except:
+        errorhandler.send_error(400, 'unknown error')
+
+    db_write['ticket'] = kwargs['id']
+    db_write['state'] = 'CREATED'
+    db_write['created'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    db_write['ats'] = None
+
+    dbhandler.insert_task(db_write)
+    return Response()
 
 @utility.add_required_headers
 def tickets_tasks_detail_GET(**kwargs):
