a74ca9b4d7b42e9ff51e23a7599fc729fbb68a6c
Adam Kucera <adam.kucera@rcesystems.cz>
2019-12-02 18:09:41 +0100

    fix for ev

--- BE/https_server/dbhandler/settings.py
+++ BE/https_server/dbhandler/settings.py
@@ -1,7 +1,2 @@
-import sys, os
-SWD = os.path.dirname(sys.argv[0])+'/'
-if SWD == '/':
-    SWD = ''
-
 DATABASE    = SWD+'../database.db' # Database file.
 INIT_SCRIPT = SWD+'../create.sql'  # Database initialisation script.
