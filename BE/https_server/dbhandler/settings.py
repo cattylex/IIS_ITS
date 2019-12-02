ba4256a474f84f9cb2424755d672e3e10e687387
Adam Kucera <adam.kucera@rcesystems.cz>
2019-12-02 17:15:07 +0100

    fix

--- BE/https_server/dbhandler/settings.py
+++ BE/https_server/dbhandler/settings.py
@@ -1,2 +1,5 @@
-DATABASE    = '../database.db' # Database file.
-INIT_SCRIPT = '../create.sql'  # Database initialisation script.
+import sys, os
+SWD = os.path.dirname(sys.argv[0])+'/'
+
+DATABASE    = SWD+'../database.db' # Database file.
+INIT_SCRIPT = SWD+'../create.sql'  # Database initialisation script.
